0145c7d34ada4ea68cc5b12a7e2b46008b29396d
Legion <ddnymlil@gmail.com>
2014-12-07 22:21:10 -0800

    Rewrote to use the standard go sql library, not a driver specific interface

--- main.go
+++ main.go
@@ -2,36 +2,60 @@ package main
 
 import (
 	"fmt"
-	"os"
-    "github.com/ziutek/mymysql/mysql"
-    _ "github.com/ziutek/mymysql/native" // Native engine
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db := mysql.New("tcp", "", "192.168.42.82:3306", "anyone", "", "phpnuke")
-	err := db.Connect()
+	db, err := sql.Open("mysql", "anyone@tcp([192.168.42.82]:3306)/phpnuke")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
-rows, _, err := db.Query("select * from nuke_bbposts")
-if err != nil {
-	panic(err)
-}
-
-for _, row := range rows {
-	for _, col := range row {
-		if col == nil {
-
-		} else {
-			colval := col.([]byte)
-			os.Stdout.Write(colval)
-			fmt.Printf("\n")
-		}
-	}
-}
-
-
-
 	fmt.Printf("It's alive!")
+
+	var (
+		id int
+		topic int
+		forum int
+		poster int
+		time int
+		ip string
+		name string
+		bbcode int
+		html int
+		smilies int
+		sig int
+		edittime []byte
+		count int
+	)
+
+	rows, err := db.Query("select * from nuke_bbposts")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	cols, err := rows.Columns()
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(cols, "\n")
+	}
+
+
+
+	for rows.Next() {
+		err := rows.Scan(&id, &topic, &forum, &poster, &time, &ip, &name, &bbcode, &html, &smilies, &sig, &edittime, &count)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", name)
+	}
+
 }
